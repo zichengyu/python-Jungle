8b266b5cbf9b26900391ba4ec3a2f12091803a6f
unknown <20160301301@NW-20160301301.neoway.com.cn>
2017-10-30 21:35:11 +0800

    Signed-off-by: unknown <20160301301@NW-20160301301.neoway.com.cn>

---
+++ ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+
+
+class JobboleSpider(scrapy.Spider):
+    name = 'jobbole'
+    allowed_domains = ['blog.jobbole.com']
+    start_urls = ['http://blog.jobbole.com/112783/']
+
+    def parse(self, response):
+        title = response.xpath('//div[@class="entry-header"]/h1/text()').extract()[0]
+        create_date = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()').extract()[0].strip().replace('·','').strip()
+        praise_nums = response.xpath("//span[contains(@class, 'vote-post-up')]/h10/text()").extract()[0]
+        fav_nums = response.xpath("//span[contains(@class, 'bookmark-btn')]/text()").extract()[0]
+        match_re = re.match(r".*(\d+).*", fav_nums)
+        if match_re:
+            fav_nums = match_re.group(1)
+
+        comment_num = response.xpath("//a[@href='#article-comment']/span").extract()[0]
+        match_re = re.match(r".*(\d+).*", comment_num)
+        if match_re:
+            comment_num = match_re.group(1)
+
+        contetn = response.xpath("//div[@class='entry']").extract()[0]
+
+        print(contetn)
+        pass
+
